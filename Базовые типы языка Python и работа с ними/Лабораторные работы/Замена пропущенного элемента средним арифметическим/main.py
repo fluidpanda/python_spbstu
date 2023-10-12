237c8b0ba0fc9cbf174b95d079d699ed77bdd31d
dmibor <fluidpandaps@protonmail.ch>
2023-10-12 19:58:22 +0300

    changes

--- Базовые типы языка Python и работа с ними/Лабораторные работы/Замена пропущенного элемента средним арифметическим/main.py
+++ Базовые типы языка Python и работа с ними/Лабораторные работы/Замена пропущенного элемента средним арифметическим/main.py
@@ -2,8 +2,9 @@ numbers = [2, -93, -2, 8, None, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -
 
 # TODO заменить значение пропущенного элемента средним арифметическим
 
-for item in numbers:
-    if item is None:
 
+for index in len(numbers):
+    if numbers[index] is None:
+        numbers.pop(numbers[index])
 
-print("Измененный список:", ...)
+print("Измененный список:", numbers)
