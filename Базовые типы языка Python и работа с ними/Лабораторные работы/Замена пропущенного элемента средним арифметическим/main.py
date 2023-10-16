5f2ae977dee3d2634d6e46622ed424668394a904
dborontov <qborrd@gmail.com>
2023-10-16 12:37:54 +0300

    upload

--- Базовые типы языка Python и работа с ними/Лабораторные работы/Замена пропущенного элемента средним арифметическим/main.py
+++ Базовые типы языка Python и работа с ними/Лабораторные работы/Замена пропущенного элемента средним арифметическим/main.py
@@ -1,19 +1,5 @@
 numbers = [2, -93, -2, 8, None, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -92, -45, 67, 53, 25]
 
-# numbers_initial_length = len(numbers)
-# numbers_without_none = []
-# index = 0
-#
-# for item in numbers:
-#     if item is None:
-#         index = numbers.index(item)
-#         numbers.pop(index)
-#
-# numbers_sum = sum(numbers)
-# numbers_average = numbers_sum / numbers_initial_length
-#
-# print(numbers.insert(index, numbers_average))
-
 numbers[4] = (sum(numbers[:4]) + sum(numbers[5:])) / len(numbers)
 
 print("Измененный список:", numbers)
--- Базовые типы языка Python и работа с ними/Лабораторные работы/Статистика посещений сайта/main.py
+++ Базовые типы языка Python и работа с ними/Лабораторные работы/Статистика посещений сайта/main.py
@@ -1,3 +1,5 @@
 users = ['user1', 'user2', 'user3', 'user1', 'user4', 'user2']
 
+value_total: dict = {"Общее количество": len(users), "Уникальные посещения": len(set(users))}
 
+print(value_total)
