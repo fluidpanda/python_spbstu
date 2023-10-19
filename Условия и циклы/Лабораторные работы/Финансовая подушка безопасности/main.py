620b7f88c0e9f2b7bc92420bccad0c1fb1d07531
dborontov <fluidpandaps@protonmail.ch>
2023-10-19 20:57:01 +0300

    changes

--- Условия и циклы/Лабораторные работы/Финансовая подушка безопасности/main.py
+++ Условия и циклы/Лабораторные работы/Финансовая подушка безопасности/main.py
@@ -7,18 +7,17 @@ increase = 0.05  # Ежемесячный рост цен
 
 # TODO Посчитайте количество  месяцев, которое можно протянуть без долгов
 
-month = 0
-banka = 0
+month = 1
 
 while money_capital > 0:
     ic(money_capital, month)
     if month == 0:
-        money_capital -= spend
         money_capital += salary
+        money_capital -= spend
         month += 1
     else:
-        money_capital -= spend * 1.05
         money_capital += salary
+        money_capital -= spend * 1.05
         month += 1
 
 print("Количество месяцев, которое можно протянуть без долгов:", month)
