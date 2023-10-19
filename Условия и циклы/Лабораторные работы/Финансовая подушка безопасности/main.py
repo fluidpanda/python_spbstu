46a64daf3324506f04ee04320915e69368695c35
dborontov <fluidpandaps@protonmail.ch>
2023-10-19 21:15:50 +0300

    changes

--- Условия и циклы/Лабораторные работы/Финансовая подушка безопасности/main.py
+++ Условия и циклы/Лабораторные работы/Финансовая подушка безопасности/main.py
@@ -11,15 +11,19 @@ month = 0
 
 while money_capital > 0:
     if month == 0:
-        month += 1
         money_capital -= spend
         money_capital += salary
+        round(money_capital, 2)
         ic(money_capital, spend, month)
-    else:
         month += 1
+    else:
         spend *= 1.05
         money_capital -= spend
+        if money_capital < 0:
+            break
         money_capital += salary
+        round(money_capital, 2)
         ic(money_capital, spend, month)
+        month += 1
 
 print("Количество месяцев, которое можно протянуть без долгов:", month)
