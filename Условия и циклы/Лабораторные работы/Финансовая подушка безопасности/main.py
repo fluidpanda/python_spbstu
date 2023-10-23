c340b1fbd58531ce46b983dd79280ac6c4525d19
dborontov <qborrd@gmail.com>
2023-10-23 17:01:31 +0300

    upload

--- Условия и циклы/Лабораторные работы/Финансовая подушка безопасности/main.py
+++ Условия и циклы/Лабораторные работы/Финансовая подушка безопасности/main.py
@@ -1,9 +1,9 @@
 from icecream import ic
 
-money_capital = 20000  # Подушка безопасности
-salary = 5000  # Ежемесячная зарплата
-spend = 6000  # Траты за первый месяц
-increase = 0.05  # Ежемесячный рост цен
+money_capital: int = 20000  # Подушка безопасности
+salary: int = 5000  # Ежемесячная зарплата
+spend: int = 6000  # Траты за первый месяц
+increase: float = 0.05  # Ежемесячный рост цен
 
 # TODO Посчитайте количество  месяцев, которое можно протянуть без долгов
 
@@ -13,17 +13,15 @@ while money_capital > 0:
     if month == 0:
         money_capital -= spend
         money_capital += salary
-        round(money_capital, 2)
-        ic(money_capital, spend, month)
         month += 1
+        spend *= 1 + increase
     else:
-        spend *= 1.05
         money_capital -= spend
+        money_capital += salary
         if money_capital < 0:
             break
-        money_capital += salary
-        round(money_capital, 2)
-        ic(money_capital, spend, month)
         month += 1
+        spend *= 1 + increase
+
 
 print("Количество месяцев, которое можно протянуть без долгов:", month)
