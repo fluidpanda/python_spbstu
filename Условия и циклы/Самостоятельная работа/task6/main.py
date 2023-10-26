6d289fa4a36e129dcf6fbbd362170b285d9028e9
dborontov <fluidpandaps@protonmail.ch>
2023-10-26 18:15:27 +0300

    changes

--- Условия и циклы/Самостоятельная работа/task6/main.py
+++ Условия и циклы/Самостоятельная работа/task6/main.py
@@ -2,8 +2,14 @@ list_numbers = [2, 90, -2, 8, -36, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38
 
 # TODO Поменяйте местами значения согласно условию
 
+index_max: int = 0
+value_max: int = list_numbers[index_max]
+
 for index, value in enumerate(list_numbers):
-    if list_numbers[index] >
+    if value >= value_max:
+        value_max = value
+        index_max = index
+
+list_numbers[index_max], list_numbers[-1] = list_numbers[-1], list_numbers[index_max]
 
 print(list_numbers)  # Ответ [2, 90, -2, 8, -36, -44, -1, -85, -14, 25, -22, -90, -100, -8, 38, -92, -45, 67, 53, 90]
-
--- Функции и методы/Функции. Создание функций/Передача аргументов в функцию и возвращаемые значения/main.py
+++ Функции и методы/Функции. Создание функций/Передача аргументов в функцию и возвращаемые значения/main.py
@@ -1,4 +1,11 @@
 # TODO Напишите функцию `multiply_numbers`
 
+def multiply_numbers(a, b):
+    return a * b
+
+
+print(multiply_numbers(2, 3))
+print(multiply_numbers(4, 5))
+print(multiply_numbers(6, 7))
 
 # TODO Перемножте с помощью функции `multiply_numbers` при пары чисел и распечатайте результаты
