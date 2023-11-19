8445c3bcc2165a0d4106d5854a6d256f1cbdc462
fluidpanda <qborrd@gmail.com>
2023-11-19 21:23:34 +0300

    game

--- game.py
+++ game.py
@@ -1,56 +1,60 @@
-board = [" " for item in range(9)]
+from typing import Any
+
+BOARD: list = [" " for item in range(0, 9)]
 
 
 def print_board():
     print("-------------")
-    print("|", board[0], "|", board[1], "|", board[2], "|")
+    print("|", BOARD[0], "|", BOARD[1], "|", BOARD[2], "|")
     print("-------------")
-    print("|", board[3], "|", board[4], "|", board[5], "|")
+    print("|", BOARD[3], "|", BOARD[4], "|", BOARD[5], "|")
     print("-------------")
-    print("|", board[6], "|", board[7], "|", board[8], "|")
+    print("|", BOARD[6], "|", BOARD[7], "|", BOARD[8], "|")
     print("-------------")
 
 
-def check_winner(player):
+def win_logic(player: Any) -> bool:
     for item in range(0, 9, 3):
-        if board[item] == board[item + 1] == board[item + 2] == player:
+        if BOARD[item] == BOARD[item + 1] == BOARD[item + 2] == player:
             return True
 
     for item in range(3):
-        if board[item] == board[item + 3] == board[item + 6] == player:
+        if BOARD[item] == BOARD[item + 3] == BOARD[item + 6] == player:
             return True
 
-    if board[0] == board[4] == board[8] == player:
+    if BOARD[0] == BOARD[4] == BOARD[8] == player:
         return True
 
-    if board[2] == board[4] == board[6] == player:
+    if BOARD[2] == BOARD[4] == BOARD[6] == player:
         return True
 
     return False
 
 
-def play_game():
-    current_player = "X"
-    game_over = False
+def main() -> None:
+    current_player: str = "X"
+    game_over_state: bool = False
 
-    while not game_over:
+    while not game_over_state:
         print_board()
-        position = int(input("Enter a position, from 1...9: ")) - 1
+        position: int = int(input("Enter a position, from 1...9: ")) - 1
 
-        if board[position] == " ":
-            board[position] = current_player
-            if check_winner(current_player):
+        if BOARD[position] == " ":
+            BOARD[position] = current_player
+            if win_logic(current_player):
                 print_board()
-                print("Player", current_player, "wins")
-                game_over = True
-            elif " " not in board:
+                print(f"Player {current_player} wins")
+                game_over_state = True
+            elif " " not in BOARD:
                 print_board()
                 print("It's a tie!")
-                game_over = True
+                game_over_state = True
             else:
                 current_player = "O" if current_player == "X" else "X"
+
         else:
             print("That position is already taken. Try again.")
 
 
-play_game()
+if __name__ == "__main__":
+    main()
